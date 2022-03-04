e72ad6a0321532e6a269cce9bbea17b2a21590ba
cptspacecadet <m@tankbottoms.xyz>
2022-03-04 20:41:19 +0500

    fix: warning in vscode

--- contracts/JBConstants.sol
+++ contracts/JBConstants.sol
@@ -2,73 +2,61 @@
 pragma solidity 0.8.6;
 
 /**
-  @notice
-  Global constants used across multiple Juicebox contracts.
+  @notice Global constants used across multiple Juicebox contracts.
 */
 library JBConstants {
-  /** 
-    @notice
-    Maximum value for reserved, redemption, and ballot redemption rates. Does not include discount rate.
+  /**
+    @notice Maximum value for reserved, redemption, and ballot redemption rates. Does not include discount rate.
   */
   uint256 public constant MAX_RESERVED_RATE = 10000;
 
   /**
-    @notice
-    Maximum token redemption rate.  
-    */
+    @notice Maximum token redemption rate.
+  */
   uint256 public constant MAX_REDEMPTION_RATE = 10000;
 
-  /** 
-    @notice
-    A funding cycle's discount rate is expressed as a percentage out of 1000000000.
+  /**
+    @notice A funding cycle's discount rate is expressed as a percentage out of 1000000000.
   */
   uint256 public constant MAX_DISCOUNT_RATE = 1000000000;
 
-  /** 
-    @notice
-    Maximum splits percentage.
+  /**
+    @notice Maximum splits percentage.
   */
   uint256 public constant SPLITS_TOTAL_PERCENT = 1000000000;
 
-  /** 
-    @notice
-    Maximum fee rate as a percentage out of 1000000000
+  /**
+    @notice Maximum fee rate as a percentage out of 1000000000
   */
   uint256 public constant MAX_FEE = 1000000000;
 
-  /** 
-    @notice
-    Maximum discount on fee granted by a gauge.
+  /**
+    @notice Maximum discount on fee granted by a gauge.
   */
   uint256 public constant MAX_FEE_DISCOUNT = 1000000000;
 
-  /** 
-    @notice
-    A thousand days in seconds.
+  /**
+    @notice A thousand days in seconds.
   */
   uint256 public constant ONE_THOUSAND_DAYS = 8640000;
 
-  /** 
-    @notice
-    Two hundred and fifty days in seconds
+  /**
+    @notice Two hundred and fifty days in seconds
   */
   uint256 public constant TWO_HUNDRED_FIFTY_DAYS = 21600000;
 
-  /** 
-    @notice
-    One hundred days in seconds
+  /**
+    @notice One hundred days in seconds
   */
   uint256 public constant ONE_HUNDRED_DAYS = 8640000;
 
-  /** 
-    @notice
-    Twenty-five days in seconds
+  /**
+    @notice Twenty-five days in seconds
   */
   uint256 public constant TWENTY_FIVE_DAYS = 2160000;
 
-  /** 
-    @notice
-    Ten days in seconds.
+  /**
+    @notice Ten days in seconds.
   */
   uint256 public constant TEN_DAYS = 864000;
 }
